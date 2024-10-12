27861e285ee56b33a018e7736adf5535f1fac5ea
yves1-357 <152566364+yves1-357@users.noreply.github.com>
2024-10-12 12:29:30 +0200

    classe currentAccount debut

---
+++ banque.cs
@@ -0,0 +1,23 @@
+﻿//Créer une classe "CurrentAccount" qui permet la gestion d'un compte courant implémentant :
+//Les propriétés publiques 
+  //  - string number, double balance (lecture seule), double CreditLine, Person Owner
+
+    // Les méthodes publiques 
+   //  - void Withdraw (double amount),
+   // - void Deposit(double amount)
+   
+   
+   class Currentaccount
+   {
+       public string number;
+       public double balance;
+       public double CreditLine;
+       public string doubleOwner;
+
+       public Currentaccount(string number, double balance, double credit)
+       {
+           
+
+       }
+   }
+ 
